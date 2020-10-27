4a00ef64d93472199dc4f118addbcabd21ed3fdd
ejmudi <truemudia@gmail.com>
2020-10-27 10:20:55 +0100

    a

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learnj React
         </a>
       </header>
     </div>
